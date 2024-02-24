534b19b3f7632174a689573b6d5e91495c1bdc1c
tanirsahoo <tanir.sahoo.2021@gmail.com>
2024-02-25 01:15:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ParkoFriend
+ This repository contains files for a software that deals with solving the parking and problems with over conjestion
---
+++ js/script_for_map.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([22.5331, 88.3336], 11);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
