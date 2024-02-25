9e8cdd00e13f9c8f83790b156ecc4f8325da34ca
tanirsahoo <tanir.sahoo.2021@gmail.com>
2024-02-25 10:45:44 +0530

    Updates

--- js/script_for_map.js
+++ js/script_for_map.js
@@ -4,11 +4,20 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let latitudes    = [22.6031, 22.5168, 22.5388] ;
-let longitudes = [88.4671, 88.3491, 88.3289] ;
+let latitudes    = [22.6031, 22.5268, 22.5388, 22.5688, 22.5188, 22.5688, 22.5988, 22.6088, 22.5388, 22.5188] ;
+let longitudes = [88.4871, 88.3491, 88.3389, 88.3989, 88.3789, 88.3789, 88.4089, 88.3789, 88.3789, 88.3989] ;
 let marker = [] ;
+
+var redIcon = new L.Icon({
+    iconUrl: 'img/parkinglogo.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
 
 let j = 0 ;
 latitudes.forEach(element => {
-    marker.push(L.marker([element , longitudes[j ++]]).addTo(map)) ;
+    marker.push(L.marker([element , longitudes[j ++]], {icon: redIcon}).addTo(map)) ;
 });
