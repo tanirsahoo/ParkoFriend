cbfd6da3945496ac0e437297d587d72d1bc9e299
tanirsahoo <tanir.sahoo.2021@gmail.com>
2024-03-04 22:47:59 +0530

    Updates

--- Landing_Page.php
+++ Landing_Page.php
@@ -5,11 +5,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+	integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
     <script src="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></script>
     <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
+	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/style_map.css">
 	<script src="js/modernizr.js"></script>
   	
